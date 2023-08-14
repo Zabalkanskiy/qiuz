04e4887753b1c2c4a634723e1a9be6f2deb65a02
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-14 13:17:17 +0300

    Add field resBackground in Question class

--- app/src/main/java/com/example/qiuz/Question.kt
+++ app/src/main/java/com/example/qiuz/Question.kt
@@ -1,4 +1,4 @@
 package com.example.qiuz
 
 data class Question(val text: String, val firstButton: String, val secondButton: String,
-                    val thirdButton: String, val fourButton : String, val answer: String)
+                    val thirdButton: String, val fourButton : String, val answer: String, val resBackground: Int = 0)
